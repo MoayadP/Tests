504f902ef2fc8b658d6105719f3e11ca03cc3ffc
moayad mansour <moayadmansour99@gmail.com>
2021-10-29 13:29:59 +0200

    sec commit

---
+++ lib/main.dart
@@ -0,0 +1,151 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: FirstPage(),
+    );
+  }
+}
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 200,
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'LANDWAYS',
+                        style: TextStyle(
+                          color: Colors.green,
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Container(
+                        alignment: Alignment.center,
+                        child: const Text(
+                          'FIND YOUR POINT OF INTEREST',
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ]),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                  height: 70,
+                  child: Container(
+                      alignment: Alignment.topLeft,
+                      child: RichText(
+                        text: const TextSpan(
+                          style: TextStyle(
+                            fontSize: 17.0,
+                            color: Colors.black,
+                          ),
+                          children: <TextSpan>[
+                            TextSpan(
+                                text:
+                                    "   We've detected your current location as",
+                                style: TextStyle(
+                                    fontSize: 19,
+                                    color: Colors.grey,
+                                    fontWeight: FontWeight.bold)),
+                            TextSpan(
+                                text: ' \n   Damascus, Syria.',
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    color: Colors.green,
+                                    fontWeight: FontWeight.bold)),
+                          ],
+                        ),
+                      ))),
+              SizedBox(
+                width: 360,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Accept'),
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.green),
+                  ),
+                ),
+              ),
+              SizedBox(
+                  height: 90,
+                  child: Container(
+                    child: const Text(
+                      'OR',
+                      style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    alignment: Alignment.center,
+                  )),
+              const SizedBox(
+                width: 360,
+                height: 50,
+                child: TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Type in your location'),
+                ),
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              SizedBox(
+                width: 360,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('DONE'),
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.green),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 50,
+                child: Container(
+                  alignment: Alignment.center,
+                  child: const Text(
+                    'You can also change your location from filters',
+                    style: TextStyle(
+                        color: Colors.grey,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
