a31c435e41721dd521974eb5b3bb0542e0807479
moayad mansour <moayadmansour99@gmail.com>
2021-11-02 17:36:45 +0200

    app-bar (TabBar)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:ui_test_1/second_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,156 +21,40 @@ class FirstPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
+    return DefaultTabController(
+      initialIndex: 0,
+      length: 3,
       child: Scaffold(
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.amber,
-          child: Text('HI'),
-          onPressed: () {
-            Navigator.of(context)
-                .push(MaterialPageRoute(builder: (_) => SecondPage()));
-          },
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 20,
+        appBar: AppBar(
+          title: const Text('TabBar Widget'),
+          bottom: const TabBar(
+            tabs: <Widget>[
+              Tab(
+                icon: Icon(Icons.cloud_outlined),
               ),
-              Container(
-                padding: EdgeInsets.all(13),
-                alignment: Alignment.topLeft,
-                child: Text(
-                  'Gallery',
-                  style: TextStyle(
-                      color: Colors.cyan.shade900,
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold),
-                ),
+              Tab(
+                icon: Icon(Icons.beach_access_sharp),
               ),
-              SizedBox(
-                  height: 120,
-                  child: Center(
-                    child: ListView.builder(
-                        padding: EdgeInsets.all(3.0),
-                        scrollDirection: Axis.horizontal,
-                        itemCount: 10,
-                        itemBuilder: (BuildContext context, int index) {
-                          return Center(
-                            child: Row(
-                              children: [
-                                SizedBox(
-                                    width: 170,
-                                    child: Card(
-                                        elevation: 10,
-                                        shadowColor: Colors.amber,
-                                        child: Stack(
-                                          children: [
-                                            Center(
-                                              child: Image.asset(
-                                                'assets/images/tree.jpg',
-                                                fit: BoxFit.fill,
-                                              ),
-                                            ),
-                                            Align(
-                                              alignment: Alignment.center,
-                                              child: Icon(
-                                                Icons.linked_camera,
-                                                size: 40,
-                                                color: Colors.amber,
-                                              ),
-                                            ),
-                                          ],
-                                        ))),
-                                const SizedBox(
-                                  width: 30,
-                                )
-                              ],
-                            ),
-                          );
-                        }),
-                  )),
-              const SizedBox(
-                height: 30,
+              Tab(
+                icon: Icon(Icons.brightness_5_sharp),
               ),
-              Container(
-                padding: EdgeInsets.all(13),
-                alignment: Alignment.topLeft,
-                child: Text(
-                  'List',
-                  style: TextStyle(
-                      color: Colors.cyan.shade900,
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold),
-                ),
-              ),
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  children: [
-                    list_cards('First List', context, Colors.black87),
-                    list_cards('Second List', context, Colors.red.shade900),
-                    list_cards('Third List', context, Colors.amber)
-                  ],
-                ),
-              )
             ],
           ),
+        ),
+        body: const TabBarView(
+          children: <Widget>[
+            Center(
+              child: Text("It's cloudy here"),
+            ),
+            Center(
+              child: Text("It's rainy here"),
+            ),
+            Center(
+              child: Text("It's sunny here"),
+            ),
+          ],
         ),
       ),
     );
   }
-
-  // ignore: non_constant_identifier_names
-  Widget list_cards(String title, BuildContext context, Color color) => Padding(
-        padding: EdgeInsets.all(20),
-        child: Card(
-          elevation: 20,
-          shadowColor: Colors.blue,
-          child: Column(
-            children: [
-              Card(
-                elevation: 20,
-                shadowColor: Colors.blue,
-                child: SizedBox(
-                  height: 40,
-                  width: 400,
-                  child: Container(
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(color: color),
-                    child: Text(
-                      title,
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(
-                  width: 400,
-                  height: MediaQuery.of(context).size.height * 50 / 100,
-                  child: ListView.builder(
-                      itemCount: 20,
-                      itemBuilder: (BuildContext context, int index) {
-                        return SizedBox(
-                          height: 40,
-                          child: Container(
-                            padding: EdgeInsets.all(6),
-                            decoration: BoxDecoration(
-                                border: Border.all(
-                                    width: 0.1, color: Colors.grey[900]!)),
-                            alignment: Alignment.center,
-                            child: Text(
-                              'Some Text',
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                          ),
-                        );
-                      })),
-            ],
-          ),
-        ),
-      );
 }
